e331b3fd52d4fb6a02f35e66bd7f126457f826d5
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2023-07-12 15:25:08 -0300

    Add files via upload

---
+++ ListTest2.py
@@ -0,0 +1,24 @@
+list = [4, 6, 2.0, 3.445, 'Pink', 101, False]
+
+# Print these values only using the position of each
+# element in the list
+
+# 2.0
+
+print()
+
+# Pink
+
+print()
+
+# 4
+
+print()
+
+# False
+
+print()
+
+# Change the 6 in the list to a different number
+
+print(list)
